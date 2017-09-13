8e4f458443079e0c73116cdf5ba07942d7d6b67f
Mahmudur Rahman <mahmud@mazegeek.com>
2017-09-13 12:40:27 +0600

    Auth middleware added in routes

--- routes/web.php
+++ routes/web.php
@@ -17,27 +17,33 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/dashboard', 'DashboardController@getDashboard')->name('dashboard');
+Route::middleware(['auth'])->group(function(){
 
-Route::resource('/dashboard/store', 'Store\StoreController');
-Route::get('store/{id}/user/manage', 'UserController@getStoreUsers')->name('store.user.manage');
-Route::post('store/{id}/user/add', 'UserController@addUserToStore')->name('store.user.add');
-Route::get('store/{store_id}/user/{user_id}/permission', 'UserController@showUsersPermission')->name('store.user.permission');
-Route::post('store/{store_id}/user/{user_id}/permission', 'UserController@updateUsersPermission')->name('store.user.permission.update');
-Route::get('/store/price/', 'Store\ItemController@getStorePrices')->name('store.price.get');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/dashboard', 'DashboardController@getDashboard')->name('dashboard');
 
-Route::get('/orders', 'Order\OrderController@getAll')->name('orders.all');
-Route::get('/orders/awaiting-payment', 'Order\OrderController@getAwaitingPaymentOrders')->name('orders.awaiting_payment');
-Route::get('/orders/awaiting-shipment', 'Order\OrderController@getAwaitingShipmentOrders')->name('orders.awaiting_shipment');
-Route::get('/orders/awaiting-order', 'Order\OrderController@getAwaitingOrderList')->name('orders.awaiting_order');
-Route::get('/orders/print-label', 'Order\OrderController@getPrintLabelList')->name('orders.print_label');
-Route::get('/orders/awaiting-tracking', 'Order\OrderController@getAwaitingTrackingList')->name('orders.awaiting_tracking');
-Route::get('/order/{id}', 'Order\OrderController@show')->name('order.show');
-Route::post('/tracking-number/save', 'Order\OrderController@saveTrackingNumber')->name('tracking_no.save');
+    Route::resource('/dashboard/store', 'Store\StoreController');
+    Route::get('store/{id}/user/manage', 'UserController@getStoreUsers')->name('store.user.manage');
+    Route::post('store/{id}/user/add', 'UserController@addUserToStore')->name('store.user.add');
+    Route::get('store/{store_id}/user/{user_id}/permission', 'UserController@showUsersPermission')->name('store.user.permission');
+    Route::post('store/{store_id}/user/{user_id}/permission', 'UserController@updateUsersPermission')->name('store.user.permission.update');
+    Route::get('/store/price/', 'Store\ItemController@getStorePrices')->name('store.price.get');
 
-Route::post('/order/invoice/save', 'Order\OrderController@saveInvoice')->name('invoice.save');
-Route::post('/order/submit', 'Order\OrderController@orderSubmit')->name('order.submit');
+    Route::get('/orders', 'Order\OrderController@getAll')->name('orders.all');
+    Route::get('/orders/awaiting-payment', 'Order\OrderController@getAwaitingPaymentOrders')->name('orders.awaiting_payment');
+    Route::get('/orders/awaiting-shipment', 'Order\OrderController@getAwaitingShipmentOrders')->name('orders.awaiting_shipment');
+    Route::get('/orders/awaiting-order', 'Order\OrderController@getAwaitingOrderList')->name('orders.awaiting_order');
+    Route::get('/orders/print-label', 'Order\OrderController@getPrintLabelList')->name('orders.print_label');
+    Route::get('/orders/awaiting-tracking', 'Order\OrderController@getAwaitingTrackingList')->name('orders.awaiting_tracking');
+    Route::get('/order/{id}', 'Order\OrderController@show')->name('order.show');
+    Route::post('/tracking-number/save', 'Order\OrderController@saveTrackingNumber')->name('tracking_no.save');
+
+    Route::post('/order/invoice/save', 'Order\OrderController@saveInvoice')->name('invoice.save');
+    Route::post('/order/submit', 'Order\OrderController@orderSubmit')->name('order.submit');
+
+});
+
+
 
 
 Route::get('/store-price/sync', function(){
